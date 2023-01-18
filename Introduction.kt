71c9e64bdee621190d0cf79bbe6760ae014b16d4
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2023-01-18 14:01:30 -0600

    Add files via upload

---
+++ Introduction.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Hello my name is Michael Tapia and I am trying to major in the Mobile Design and Development Associates Degree")
+    println("and the reason why I decided to take this class was because I wanted to learn how to code in Kotlin, code better, and make apps on Androids.")
+    println("One of my goals for this year is to learn how to code with Kotlin, Swift, JavaScript, and C++ before the end of this year and")
+    println("My 2 favorite hobbies are playing video games and working out but recently I've been learning how to use blender to create 3D models")
+    println("to import them into Unreal Engine to mess around and see if I can learn how to make a video game.")
+}
