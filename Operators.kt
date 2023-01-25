0bbe2b9eb45ea777554f7875d336057e792b7b1e
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2023-01-25 13:12:55 -0600

    Add files via upload

---
+++ Operators.kt
@@ -0,0 +1,24 @@
+fun main() {
+    var a = 9
+    var b = 11
+    var c = 20
+    // First Operator
+
+    var ans1 = a + b
+    println(ans1)
+    // Second Operator
+
+    var ans2 = a >= c
+    println(ans2)
+    // Third Operator
+    var ans3 = c % a
+    println(ans3)
+    // Fourth Operator
+    var ans4 = 15
+    ans4 -= 6
+    println(ans4)
+    // Fifth Operator
+    var ans5 = 9
+    ans5++
+    println(ans5)
+}
