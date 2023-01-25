63f9bf771d4604f4681919e016424c1a93e740bd
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2023-01-25 13:39:43 -0600

    Add files via upload

---
+++ Variables.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val myByte: Byte = 9
+    val myShort: Short = 90
+    val myInt: Int = 900
+    val myLong: Long = 900000
+    val myFloat: Float = 3.14F
+    val myDouble: Double = 31.415
+    val myBoolean: Boolean = true
+    val myChar = "Hello World"
+
+    println("Byte: $myByte \nShort: $myShort \nInteger: $myInt \nLong: $myLong")
+    println("Float: $myFloat \nDouble: $myDouble \nBoolean: $myBoolean \nCharacter: $myChar")
+ 
+}
