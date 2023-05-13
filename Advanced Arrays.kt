a218ffa82b5c8bd5066d2a2ecd47733dc6f10ead
Michael Tapia <89033407+MikeWazowski99@users.noreply.github.com>
2023-05-12 20:55:27 -0500

    Advanced Arrays.kt

--- Advanced Arrays.kt
+++ Advanced Arrays.kt
@@ -12,21 +12,22 @@ fun main() {
         println(singlePart)
     }
 
-    val combArray = arrayOf(dimArray, quant)
+        val combArray = arrayOf(dimArray, quant)
 
-    val rowCount = combArray[0].size - 1
+        val rowCount = combArray[0].size - 1
 
-    for (i in 0..rowCount) {
-        val item = combArray[0][i]
-        var qty = combArray[1][i].toInt()
+        for (i in 0..rowCount) {
+            val item = combArray[0][i]
+            var qty = combArray[1][i].toInt()
 
-        println("Please enter the total amount you would like to add or subtract from the $item storage")
-        println("In the storage: $item has $qty items.")
+            println("Please enter the total amount you would like to add or subtract from the $item storage")
+            println("In the storage: $item has $qty items.")
 
-        val qtyToAdd = readLine()?.toIntOrNull() ?: 0
-        qty += qtyToAdd
-        qty = qty.coerceAtLeast(0)
+            val qtyToAdd = readLine()?.toIntOrNull() ?: 0
+            qty += qtyToAdd
+            qty = qty.coerceAtLeast(0)
 
-        println("New quantity for $item: $qty\n")
+            println("New quantity for $item: $qty\n")
+        }
     }
 }
